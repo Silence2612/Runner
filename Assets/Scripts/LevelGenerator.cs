7a118658f4457fcb814675998f3b8934f3b6457e
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-03-18 17:24:39 +0530

    refactored code

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,21 +9,33 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
+        SpawnChunks();
+    }
+
+    private void SpawnChunks()
+    {
         for (int i = 0; i < StartingChunksAmount; i++)
         {
-            float spawnPositionZ;
-
-            if (i == 0) 
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else 
-            {
-                spawnPositionZ = transform.position.z + (i * chunklength);
-            }
+            float spawnPositionZ = ChunkSpawnPoint(i);
 
             Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
             Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
         }
+    }
+
+    private float ChunkSpawnPoint(int i)
+    {
+        float spawnPositionZ;
+
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i * chunklength);
+        }
+
+        return spawnPositionZ;
     }
 }
