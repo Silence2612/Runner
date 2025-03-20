836525e29cb90e37f43ea8d3cac6cca83c0cc587
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-03-20 23:51:12 +0530

    added random number of fences

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
@@ -12,8 +14,17 @@ public class Chunk : MonoBehaviour
 
     void SpawnFence() 
     {
-        int randomLaneIndex = Random.Range(0, lanes.Length);
-        Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity , this.transform);
+        List<int> availableLanes = new List<int> {0,1,2};
+        int fences2spawn = Random.Range(0,3);
+        for (int i = 0; i < fences2spawn; i++)
+        {
+            int randomLaneIndex = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLaneIndex];
+            availableLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity , this.transform);
+        }
+
     }
 }
