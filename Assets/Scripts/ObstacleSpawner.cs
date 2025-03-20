90b0abdaee8f87a100deaf08b31bc0aa94dd36fa
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-03-20 23:41:12 +0530

    added fences

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -6,6 +6,7 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstacleSpawnTime = 1f;
     [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f;
 
     void Start()
     {
@@ -17,8 +18,9 @@ public class ObstacleSpawner : MonoBehaviour
         while (true)
         {
             GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0,obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth,spawnWidth),transform.position.y,transform.position.z);
             yield return new WaitForSeconds(obstacleSpawnTime);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation , obstacleParent);
         }
     }
 
